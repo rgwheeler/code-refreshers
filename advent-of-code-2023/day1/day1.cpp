3f59cbbad07bae5132f70bd2d65d60ffa6f53143
rgwheeler <142755293+rgwheeler@users.noreply.github.com>
2024-11-18 17:08:48 -0600

    Created folders and day 1

---
+++ advent-of-code-2023/day1/day1.cpp
@@ -0,0 +1,17 @@
+#include <stdio>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+    ifstream infile;
+    infile.open(input.txt);
+
+    string str;
+
+    if (infile.is_open()) {
+        
+    }
+
+
+}
